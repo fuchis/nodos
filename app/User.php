2adfd6b1f469aced7b4d6e6b4859cb5a0122e818
fuchis <ivan.lopez3k@gmail.com>
2018-08-09 22:01:00 -0500

    edit

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-     //PUTO EL QUE LO LEAdfdf
+     //PUTO EL QUE LO LEA
     protected $fillable = [
         'name', 'email', 'password',
     ];
